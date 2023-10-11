15cd496f9fb38cd1429be7edf617e3557740f5f7
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-10-11 19:14:05 +0200

    add sort, pagination, etc

--- src/graphql/Link.ts
+++ src/graphql/Link.ts
@@ -1,7 +1,15 @@
-import { extendType, objectType, nonNull, stringArg, arg } from "nexus";
-import { NexusGenObjects } from "../../nexus-typegen";
-import { GraphQLResolveInfo } from "graphql";
-import { MaybePromise } from "nexus/dist/typegenTypeHelpers";
+import {
+  extendType,
+  objectType,
+  nonNull,
+  stringArg,
+  intArg,
+  inputObjectType,
+  enumType,
+  arg,
+  list,
+} from "nexus";
+import { Prisma } from "@prisma/client";
 
 export const Link = objectType({
   name: "Link",
@@ -32,10 +40,40 @@ export const Link = objectType({
 export const LinkQuery = extendType({
   type: "Query",
   definition(t) {
-    t.nonNull.list.nonNull.field("feed", {
-      type: "Link",
-      resolve(parent, args, context) {
-        return context.prisma.link.findMany();
+    t.nonNull.field("feed", {
+      type: "Feed",
+      args: {
+        filter: stringArg(),
+        skip: intArg(),
+        take: intArg(),
+        orderBy: arg({ type: list(nonNull(LinkOrderByInput)) }),
+      },
+      async resolve(parent, args, context) {
+        const where = args.filter
+          ? {
+              OR: [
+                { description: { contains: args.filter } },
+                { url: { contains: args.filter } },
+              ],
+            }
+          : {};
+
+        const links = await context.prisma.link.findMany({
+          where,
+          skip: args?.skip as number | undefined,
+          take: args?.take as number | undefined,
+          orderBy: args?.orderBy as
+            | Prisma.Enumerable<Prisma.LinkOrderByWithRelationInput>
+            | undefined,
+        });
+
+        const count = await context.prisma.link.count({ where });
+        const id = `main-feed:${JSON.stringify(args)}`;
+        return {
+          links,
+          count,
+          id,
+        };
       },
     });
   },
@@ -67,5 +105,28 @@ export const LinkMutation = extendType({
         return newLink;
       },
     });
+  },
+});
+
+export const LinkOrderByInput = inputObjectType({
+  name: "LinkOrderByInput",
+  definition(t) {
+    t.field("description", { type: Sort });
+    t.field("url", { type: Sort });
+    t.field("createdAt", { type: Sort });
+  },
+});
+
+export const Sort = enumType({
+  name: "Sort",
+  members: ["asc", "desc"],
+});
+
+export const Feed = objectType({
+  name: "Feed",
+  definition(t) {
+    t.nonNull.list.nonNull.field("links", { type: Link });
+    t.nonNull.int("count");
+    t.id("id");
   },
 });
